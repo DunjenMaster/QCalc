f05697e4488b3884c7fe8a3a795eb8a2b48ab665
DunjenMaster <developmentwithutkarsh@gmail.com>
2025-11-23 22:38:18 +0530

    Test Case for Basic Airthmatic Operation

--- src/test/java/StandardCalculatorTest.java
+++ src/test/java/StandardCalculatorTest.java
@@ -1,3 +1,32 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.qcalc.StandardCalculator;
+
 public class StandardCalculatorTest {
+    private StandardCalculator standardCalculator;
+
+    @BeforeEach
+    public void setup(){
+        standardCalculator = new StandardCalculator();
+    }
+
+
+    @Test
+    @DisplayName("Testing Addition Operation")
+    public void testAdditionOperation(){
+        standardCalculator.add(25,5);
+        int actualResult = standardCalculator.getResult();
+        Assertions.assertEquals(30, actualResult);
+    }
+
+    @Test
+    @DisplayName("Testing Substraction Operation")
+    public void testSubstractionOperation(){
+        standardCalculator.substract(24,12);
+        int actualResult = standardCalculator.getResult();
+        Assertions.assertEquals(12, actualResult);
+    }
 
 }
