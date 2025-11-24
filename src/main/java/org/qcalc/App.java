b17182a4223f961cee4457dfd145b094df065329
DunjenMaster <developmentwithutkarsh@gmail.com>
2025-11-24 20:17:40 +0530

    Implementation has both int and double!

--- src/main/java/org/qcalc/App.java
+++ src/main/java/org/qcalc/App.java
@@ -5,16 +5,16 @@ public class App {
 
         StandardCalculator calc = new StandardCalculator();
 
-        calc.add(10, 10);
+        calc.add(10, 10); // int version
         calc.printResult();
 
-        calc.substract(20, 8);
+        calc.subtract(20.0, 8.0); // double version
         calc.printResult();
 
-        calc.multiply(5,4);
+        calc.multiply(5.0,4.0); // double version
         calc.printResult();
 
-        calc.divide(10,2);
+        calc.divide(10,2); // int version
         calc.printResult();
     }
 }
