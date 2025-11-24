eacc57525aaec1a3d845f8afa6750257406571bd
DunjenMaster <developmentwithutkarsh@gmail.com>
2025-11-24 19:37:39 +0530

    Implemented Double data type, avoiding duplication by re-using!

--- src/main/java/org/qcalc/StandardCalculator.java
+++ src/main/java/org/qcalc/StandardCalculator.java
@@ -3,34 +3,51 @@ package org.qcalc;
 public class StandardCalculator {
 
 
-    private int result;
+    private double result;
 
     public void printResult() {
         System.out.println("Calculation Result: " + result);
     }
 
-    public int getResult() {
+    public double getResult() {
         return result;
     }
 
-    public void setResult(int value) {
+    public void setResult(double value) {
         this.result = value;
     }
 
     public void add(int num1, int num2) {
-        result = num1 + num2;
+        add((double)num1, (double)num2);
     }
 
-    public void substract(int num1, int num2) {
-        result = num1 - num2;
+    public void subtract(int num1, int num2) {
+        subtract((double)num1, (double)num2);
     }
 
     public void multiply(int num1, int num2) {
-        result = num1 * num2;
+        multiply((double) num1, (double) num2);
     }
 
     public void divide(int num1, int num2) {
-        result = num1 / num2;
+        divide((double)num1, (double)num2);
     }
 
+
+    // Implementing Double Data Type for above arithmetic operations as main logic and avoid duplicate methods
+    public void add(double num1, double num2){
+        result = num1 + num2;
+    }
+
+    public void subtract(double num1, double num2){
+        result = num1 - num2;
+    }
+
+    public void multiply(double num1, double num2){
+        result = num1 * num2;
+    }
+
+    public void divide(double num1, double num2){
+        result = num1 / num2;
+    }
 }
